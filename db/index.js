a735b388232125cbca18ab37a7cc1df27cc73dc5
AmienulRana <amienulrana@gmail.com>
2022-09-15 15:00:21 +0700

    mengubah pesan dari log ketika koneksi mongodb berhasil

--- db/index.js
+++ db/index.js
@@ -4,7 +4,13 @@ mongoose
   .connect(MONGODB_URL, {
     useUnifiedTopology: true,
   })
-  .then(() => console.log("mongodb success connected"))
+  .then(() => {
+    if (process.env.MODE === "prod") {
+      console.log("success connected mongodb to cluster ");
+    } else {
+      console.log("mongodb success connected to local db");
+    }
+  })
   .catch((err) => {
     console.log(err);
     console.log("failed mongodb connected");
